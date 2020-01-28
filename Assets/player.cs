fd4013d7778ae28002bfe754292de836d44d9842
viviria <basel.2.pi6@gmail,com>
2020-01-29 02:13:28 +0900

    移動アニメーションを仮実装．

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator animator = null;
+    private Vector3 moveVec;
+    private float moveTime = 0.25f;
+    private float moveDeltaTime = 0.0f;
+    private bool isMove = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMove)
+        {
+            if (moveDeltaTime <= moveTime)
+            {
+                transform.position = transform.position + moveVec * Time.deltaTime;
+                moveDeltaTime += Time.deltaTime;
+            }
+            else
+            {
+                isMove = false;
+                this.animator.SetTrigger("idleTrigger");
+            }
+        }
     }
 
     public void leftButtonDown()
@@ -36,11 +54,12 @@ public class Player : MonoBehaviour
 
     public void upButtonDown()
     {
-        var pos = transform.position;
-        pos.z += 1.0f;
+        isMove = true;
+        moveDeltaTime = 0.0f;
+        moveVec = new Vector3(0, 0, 1.0f / moveTime);
 
-        transform.position = pos;
         transform.rotation = Quaternion.Euler(0, 0, 0);
+        this.animator.SetTrigger("walkTrigger");
     }
 
     public void downButtonDown()
