79be9be5e88bfd8e743f5f0e8cfbb28a810e3854
viviria <basel.2.pi6@gmail,com>
2020-01-29 03:26:51 +0900

    移動アニメーションを実装．

---
+++ Assets/Sript/CallbackManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Common
+{
+    public class CallbackManager
+    {
+        public bool isExecute_ = false;
+        private float endTime_ = 0.0f;
+        private float elapsedTime_ = 0.0f;
+        private Action<float> action_ = null;
+        private Action endCallback_ = null;
+
+        public CallbackManager(float endTime, Action<float> action, Action endCallback)
+        {
+            endTime_ = endTime;
+            action_ = action;
+            endCallback_ = endCallback;
+        }
+
+        public void Start()
+        {
+            isExecute_ = true;
+            elapsedTime_ = 0.0f;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (isExecute_)
+            {
+                if (elapsedTime_ <= endTime_)
+                {
+                    elapsedTime_ += deltaTime;
+                    action_?.Invoke(deltaTime);
+                }
+                else
+                {
+                    isExecute_ = false;
+                    endCallback_?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Sript/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator = null;
+    private Vector3 moveVec;
+    private float moveTime = 0.25f;
+    private float moveDeltaTime = 0.0f;
+    private bool isMove = false;
+    private CallbackManager callbackManager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (callbackManager != null)
+        {
+            callbackManager.Update(Time.deltaTime);
+        }
+    }
+    
+    void moveAction(Vector3 moveVec)
+    {
+        callbackManager = new CallbackManager(moveTime,
+            deltaTime => {
+                transform.position = transform.position + moveVec * deltaTime / moveTime;
+            },
+            () => {
+                this.animator.SetTrigger("idleTrigger");
+                this.callbackManager = null;
+            }
+        );
+
+        this.animator.SetTrigger("walkTrigger");
+        callbackManager.Start();
+    }
+
+    public void moveButtonDown(int moveDir)
+    {
+        float rotate = 0.0f, dx = 0.0f, dz = 0.0f;
+
+        switch (moveDir)
+        {
+            case 0: // UP
+                dz = 1.0f;
+                break;
+            case 1: // RIGHT UP
+                rotate = 45.0f;
+                dx = 1.0f;
+                dz = 1.0f;
+                break;
+            case 2: // RIGHT
+                rotate = 90.0f;
+                dx = 1.0f;
+                break;
+            case 3: // RIGHT DOWN
+                rotate = 135.0f;
+                dx = 1.0f;
+                dz = -1.0f;
+                break;
+            case 4: // DOWN
+                rotate = 180.0f;
+                dz = -1.0f;
+                break;
+            case 5: // LEFT DOWN
+                rotate = -135.0f;
+                dx = -1.0f;
+                dz = -1.0f;
+                break;
+            case 6: // LEFT
+                rotate = -90.0f;
+                dx = -1.0f;
+                break;
+            case 7: // LEFT UP
+                rotate = -45.0f;
+                dx = -1.0f;
+                dz = 1.0f;
+                break;
+        }
+
+        transform.rotation = Quaternion.Euler(0, rotate, 0);
+        moveAction(new Vector3(dx, 0, dz));
+    }
+}
