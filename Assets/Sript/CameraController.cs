150db83e34b4d468462fade943c4d92a680c5dc8
viviria <basel.2.pi6@gmail,com>
2020-01-30 00:11:57 +0900

    カメラの追従を実装．

---
+++ Assets/Sript/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player = null;
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player");
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        transform.position = playerPos + startPos;
+    }
+}
