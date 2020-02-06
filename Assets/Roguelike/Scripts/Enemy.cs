406528955cc22256429c28f313a04d564f598fa2
viviria <vivivi.viviria@gmail.com>
2020-02-07 03:39:37 +0900

    Enemy スクリプトを作成．マスタデータの読み込みを実装．

---
+++ Assets/Roguelike/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roguelike {
+  public class Enemy : MonoBehaviour
+  {
+    public int id_ = -1;
+    
+    [SerializeField]
+    private Master.EnemyMaster master_ = null;
+
+    private Master.Entity.EnemyMasterEntity data_ = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+      data_ = master_.data.Find(x => x.id == id_);
+      Debug.Log(data_.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  }
+}
